974e41a5e832a43d5c11ea50f96336d329b68b05
Jim Shaw <jimxshaw@gmail.com>
2017-06-19 17:16:47 -0500

    Added productResource.js module that works with common.services.js module. Added common.services module as a dependency in app.js module.

--- AWK.Client/common/common.services.js
+++ AWK.Client/common/common.services.js
@@ -1,8 +1,11 @@
 ﻿(function () {
     "use strict";
 
+    // Create the module and add dependency modules.
+    // Defines a constant for module.
     angular
-        .module("common.services", ["ngResource"])
+        .module("common.services",
+                    ["ngResource"])
         .constant("appSettings",
         {
             serverPath: "http://localhost:53654/"
---
+++ AWK.Client/common/productResource.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    "use strict";
+
+    // Custom service that communicates with the backend web api service.
+    // Looks up the common.services module, registers the custom factory
+    // service with that module.
+    angular
+        .module("common.services")
+        .factory("productResource",
+                    ["$resource", "appSettings", productResource]);
+
+    // Here's the actual factory function named productResource.
+    function productResource($resource, appSettings) {
+        return $resource(appSettings.serverPath + "/api/products/:id");
+    }
+
+
+})();
