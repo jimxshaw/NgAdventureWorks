d3321e665139d7c65ad4652a4f870bddbfb4aecc
Jim Shaw <jimxshaw@gmail.com>
2017-06-19 17:04:43 -0500

    Started implementing common.services.js module in Client project.

--- AWK.Client/common/common.services.js
+++ AWK.Client/common/common.services.js
@@ -1,1 +1,10 @@
-﻿
+﻿(function () {
+    "use strict";
+
+    angular
+        .module("common.services", ["ngResource"])
+        .constant("appSettings",
+        {
+            serverPath: "http://localhost:53654/"
+        });
+})();
